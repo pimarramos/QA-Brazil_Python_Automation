e8a05c6bca87967ea391f5d7a3a1d196021e34ad
pimarramos <pimar.ramos@gmail.com>
2026-04-13 15:31:37 -0300

    Projeto Sprint 7

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # Arquivo com constantes
+URBAN_ROUTES_URL = 'https://cnt-d4bd4c75-930b-43d7-9ff4-cfa3c96d4df4.containerhub.tripleten-services.com?lng=pt'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
--- main.py
+++ main.py
@@ -1,2 +1,56 @@
+import numbers
+import data
+import helpers
+
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print(
+                "Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução."
+            )
+
+    def test_set_route(self):
+        # adicionar em S8
+        print("Função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # adicionar em S8
+        print("Função criada para selecionar o plano")
+        pass
+
+    def test_fill_phone_number(self):
+        # adicionar em S8
+        print("Função criada para preencher o número de telefone")
+        pass
+
+    def test_fill_card(self):
+        # adicionar em S8
+        print("Função criada para preencher os dados do cartão")
+        pass
+
+    def test_comment_for_driver(self):
+        # adicionar em S8
+        print("Função criada para adicionar comentário para o motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # adicionar em S8
+        print("Função criada para solicitar cobertor e lenços")
+        pass
+
+    def test_order_2_ice_creams(self):
+        numbers_of_ice_creams = 2
+        for count in range(numbers_of_ice_creams):
+            # adicionar em S8
+            print(f"Função criada para adicionar o {count + 1}º sorvete ao pedido")
+        pass
+
+    def test_car_search_model_appears(self):
+        # adicionar em S8
+        print("Função criada para verificar se o modelo de carro aparece na busca")
+        pass
